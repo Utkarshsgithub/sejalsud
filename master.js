c3ae16f0e2fbf6e47cb124127e4d8e0db4357545
Utkarsh Singh <93197668+Utkarshsgithub@users.noreply.github.com>
2022-01-28 16:18:10 +0530

    Add files via upload

---
+++ master.js
@@ -0,0 +1,13 @@
+var i = 0;
+var txt = 'finance'; /* The text */
+var speed = 150; /* The speed/duration of the effect in milliseconds */
+
+function typeWriter() {
+  if (i < txt.length) {
+    document.getElementById("demo").innerHTML += txt.charAt(i);
+    i++;
+    setTimeout(typeWriter, speed);
+  }
+}
+
+typeWriter()
